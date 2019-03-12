b3c83915952f3e630a65aae9e308b822b8cdacd4
EunSong <e.song925@gmail.com>
2019-03-12 20:50:32 +0900

    ??

---
+++ src/test/jsp/study/db/DBConHome.java
@@ -0,0 +1,43 @@
+package test.jsp.study.db;
+
+public class DBConHome {
+	private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+	private static final String USER = "OSFU";
+	private static final String PASSWORD = "k0908";
+	private static final String DRIVER = "oracle.jdbc.OracleDriver";
+	private static Connection con = null;
+	
+	private static void open() {
+		try {
+			Class.forName(DRIVER);
+			con = DriverManager.getConnection(URL,USER,PASSWORD);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static Connection getCon() {
+		if(con==null) {
+			open();
+		}
+		return con;
+	}
+	
+	public static void close() {
+		if(con!=null) {
+			try {
+				if(!con.isClosed()) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		con = null;
+	}
+}
